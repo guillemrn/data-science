232617669c559d1c37d3354db0f77c8da39cc4c6
Guillermo Moreno <guillermolpzm@gmail.com>
2021-06-22 19:26:53 -0500

    python excersices

---
+++ python_intermedio/python_excersices/excercise6.py
@@ -0,0 +1,9 @@
+def main():
+    usr_name = input("What is your full name?: ")
+    print(usr_name.lower())
+    print(usr_name.upper())
+    print(usr_name.capitalize())
+
+
+if __name__ == '__main__':
+    main()
---
+++ python_intermedio/python_excersices/excercise7.py
@@ -0,0 +1,6 @@
+def main():
+    usr_name = input("What is your name?: ")
+
+
+if __name__ == '__main__':
+    main()
