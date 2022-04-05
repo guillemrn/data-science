8e3cf96c7d66c2a63ec013e60e012448f9c381e9
Guillermo Moreno <guillermo@preyhq.com>
2022-04-05 12:28:27 -0500

    finished

--- Curso-POO-Uber/Python/main.py
+++ Curso-POO-Uber/Python/main.py
@@ -1,11 +1,14 @@
 from car import Car
 from account import Account
 from User import User
+from Driver import Driver
 
 
 if __name__ == "__main__":
     car = Car("ADF876", Account("Gabriela Quezada", "INE"))
     user = User("Francisco", "INE")
+    driver = Driver("Francisco", "INE")
     print(vars(car))
     print(vars(car.driver))
     print(vars(user))
+    print(vars(driver))
