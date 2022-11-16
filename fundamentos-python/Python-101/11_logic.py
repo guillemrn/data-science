77ad46a766d278f1cf00f9f2ebd338570e6092f5
Guillermo Moreno <guillermolpzm@gmail.com>
2022-11-15 19:42:15 -0600

    11_logic.py

--- fundamentos-python/Python-101/11_logic.py
+++ fundamentos-python/Python-101/11_logic.py
@@ -8,5 +8,15 @@ print("False and False =>", False and False)
 print(10 > 5 and 5 < 10)
 print(10 > 5 and 5 > 10)
 
-stock = int(input("Ingrese un numero de stock => "))
-print(stock >= 100 and stock <= 1000)
+# stock = int(input("Ingrese un numero de stock => "))
+# print(stock >= 100 and stock <= 1000)
+
+# or
+print("OR")
+print("True or True =>", True or True)
+print("True or False =>", True or False)
+print("False or True =>", False or True)
+print("False or False =>", False or False)
+
+role = input("Digita el rol => ")
+print(role == 'admin' or role == 'seller')
