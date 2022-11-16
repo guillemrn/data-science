b02b1e25b55d32aee2519149440a875322378995
Guillermo Moreno <guillermolpzm@gmail.com>
2022-11-15 19:55:23 -0600

    condicionales

---
+++ fundamentos-python/Python-101/13_if.py
@@ -0,0 +1,20 @@
+if True:
+    print("Deberia ejecutarse")
+
+if False:
+    print("Nunca se ejecuta")
+
+'''
+pet = input("Cual es tu mascota favorita:? ")
+if pet == "perro":
+    print("Los perros son geniales")
+
+if pet == "gato":
+    print("Los gatos son geniales")
+'''
+
+stock = int(input("Digita el stock: "))
+if stock >= 100 and stock <= 1000:
+    print("El stock es correcto")
+else:
+    print("El stock es incorrecto")
